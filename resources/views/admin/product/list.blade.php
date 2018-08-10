dae76f83e018d6526efc2d313e1681f169040c98
nnt2612 <33348945+nnt2612@users.noreply.github.com>
2018-08-10 20:54:20 +0700

    Thêm section và sửa lại giao diện

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -1,72 +1,75 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2>List Product</h2>
-                <div class="table-responsive">
-                    <table id="mytable" class="table table-bordered table-striped">
-                        <thead>
-                            <th class="col-md-1"></th>
-                            <th class="col-md-1">Name</th>
-                            <th class="col-md-1">Price</th>
-                            <th class="col-md-1">Images</th>
-                            <th class="col-md-1">Overview</th>
-                            <th class="col-md-2">Description</th>
-                            <th class="col-md-1">Category</th>
-                            <th class="col-md-1">Brand</th>
-                            <th class="col-md-3">Action</th>
-                        </thead>
-                        <tbody>
-                        @foreach($list_obj as $item)
-                            <tr class="row" id="row-item-{{$item->id}}">
-                                <td class="col-md-1 text-center">
-                                    <input type="checkbox" class="check-item">
-                                </td>
-                                <td class="col-md-1">{{$item->id}}</td>
-                                <td class="col-md-1">{{$item->name}}</td>
-                                <td class="col-md-1">{{$item->price}}</td>
-                                <td class="col-md-1">
-                                    <div class="card"
-                                         style="background-image: url('{{$item->images}}'); background-size: cover; width: 60px; height: 60px;">
-                                    </div>
-                                </td>
-                                <td class="col-md-1">{{$item->overview}}</td>
-                                <td class="col-md-2">{{$item->description}}</td>
-                                <td class="col-md-1">{{$item->category}}</td>
-                                <td class="col-md-1">{{$item->brand}}</td>
-                                <td class="col-md-3">
-                                    <a href="#" class="btn btn-link btn-quick-edit">Quick Edit</a>&nbsp;&nbsp;
-                                    <a href="/admin/bakery/edit/{{$item -> id}}" class="btn btn-link btn-edit">Edit</a>&nbsp;&nbsp;
-                                    <a href="#" id="{{$item-> id}}" class="btn btn-link btn-delete">Delete</a>
-                                </td>
+@extends('layouts.master)
+@section('page-title', 'List Article - Admin Page')
+<link href="css/style.css" rel='stylesheet' type='text/css' />//để sang bên master layout hộ c
+@section('content')
+    <section id="main-content">
+        <section class="wrapper">
+            <div class="table-agile-info">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        List Article
+                    </div>
+                    <div>
+                        <table class="table" ui-jq="footable" ui-options='{
+                            "paging": {
+                              "enabled": true
+                            },
+                            "filtering": {
+                              "enabled": true
+                            },
+                            "sorting": {
+                              "enabled": true
+                            }}'>
+                            <thead>
+                            <tr>
+                                <th data-breakpoints="xs"></th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Images</th>
+                                <th>Overview</th>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Brand</th>
+                                <th data-breakpoints="xs sm md" data-title="DOB">Action</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-md-8 form-inline">
-                    <div class="form-check mb-2">
-                        <input class="form-check-input" type="checkbox" value="" id="check-all">
-                        <label class="form-check-label" for="defaultCheck1">Check All</label>
+                            </thead>
+                            <tbody>
+                            @foreach($list_obj as $item)
+                                <tr class="row" id="row-item-{{$item->id}}">
+                                    <td>
+                                        <input type="checkbox" class="check-item">
+                                    </td>
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->name}}</td>
+                                    <td>{{$item->price}}</td>
+                                    <td>
+                                        <div class="card"
+                                             style="background-image: url('{{$item->images}}'); background-size: cover; width: 60px; height: 60px;">
+                                        </div>
+                                    </td>
+                                    <td>{{$item->overview}}</td>
+                                    <td>{{$item->description}}</td>
+                                    <td>{{$item->category}}</td>
+                                    <td>{{$item->brand}}</td>
+                                    <td>
+                                        <a href="#" class="btn btn-link btn-quick-edit">Quick Edit</a>&nbsp;&nbsp;
+                                        <a href="/admin/bakery/edit/{{$item -> id}}" class="btn btn-link btn-edit">Edit</a>&nbsp;&nbsp;
+                                        <a href="#" id="{{$item-> id}}" class="btn btn-link btn-delete">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="form-group mx-sm-3 mb-2">
-                        <span>Delete All</span>
+                    <div class="col-md-8 form-inline">
+                        <div class="form-check mb-2">
+                            <input class="form-check-input" type="checkbox" value="" id="check-all">
+                            <label class="form-check-label" for="defaultCheck1">Delete All</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2" id="btn-apply">Submit</button>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2" id="btn-apply">Submit</button>
                 </div>
             </div>
-        </div>
-    </div>
-</body>
-</html>
+        </section>
+    </section>
+@endsection
