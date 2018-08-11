a507fb0ab31a0dc583501c23e51b08e36ff52483
nnt2612 <33348945+nnt2612@users.noreply.github.com>
2018-08-11 14:47:43 +0700

    Tạo view cho brand

---
+++ resources/views/admin/brand/list.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.master)
+@section('page-title', 'List Brand - Admin Page')
+<link href="css/list.css" rel='stylesheet' type='text/css' />//để sang bên master layout hộ c
+@section('content')
+    <section id="main-content">
+        <section class="wrapper">
+            <div class="table-agile-info">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        List Brand
+                    </div>
+                    <div class="col-md-8 form-inline">
+                        <div class="form-group mx-sm-4 mb-3">
+                            <label for="chooseCategory">Brand</label>
+                            <select id="select-action" class="form-control">
+                                <option selected value="0">Brand</option>
+                                <option value="1">Urban Decay</option>
+                                <option value="2">NARS</option>
+                                <option value="3">Kylie Cosmetics</option>
+                                <option value="4">NYX</option>
+                                <option value="5">Maybelline</option>
+                                <option value="6">Stila</option>
+                                <option value="7">MAC</option>
+                                <option value="8">ColourPop</option>
+                                <option value="9">Dior</option>
+                                <option value="10">Anastasia Beverly Hills</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <table class="table" ui-jq="footable" ui-options='{
+                            "paging": {
+                              "enabled": true
+                            },
+                            "filtering": {
+                              "enabled": true
+                            },
+                            "sorting": {
+                              "enabled": true
+                            }}'>
+                            <thead>
+                                <tr>
+                                    <th data-breakpoints="xs"></th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th data-breakpoints="xs">Images</th>
+                                    <th data-breakpoints="xs sm md" data-title="DOB">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($list_obj as $item)
+                                <tr class="row" id="row-item-{{$item->id}}">
+                                    <td>
+                                        <input type="checkbox" class="check-item">
+                                    </td>
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->name}}</td>
+                                    <td>{{$item->description}}</td>
+                                    <td>
+                                        <div class="card"
+                                             style="background-image: url('{{$item->images}}'); background-size: cover; width: 60px; height: 60px;">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-link btn-quick-edit">Quick Edit</a>&nbsp;&nbsp;
+                                        <a href="/admin/bakery/edit/{{$item -> id}}" class="btn btn-link btn-edit">Edit</a>&nbsp;&nbsp;
+                                        <a href="#" id="{{$item-> id}}" class="btn btn-link btn-delete">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    </div>
+                    <div class="col-md-8 form-inline">
+                        <div class="form-check mb-2">
+                            <input class="form-check-input" type="checkbox" value="" id="check-all">
+                            <label class="form-check-label" for="defaultCheck1">Check All</label>
+                        </div>
+                        <div class="form-group mx-sm-3 mb-2">
+                            <select id="select-action" class="form-control">
+                                <option selected value="0">Action</option>
+                                <option value="1">Delete All</option>
+                                <option value="2">Another Action</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2" id="btn-apply">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+@endsection
