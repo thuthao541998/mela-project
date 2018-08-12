726cd345352c8ae8df3c92ce5735c882409b99cb
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-08-12 11:22:45 +0700

    Thêm link cdn cho bootstrap 4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         } </script>
     <!-- bootstrap-css -->
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
     <!-- //bootstrap-css -->
     <!-- Custom CSS -->
     <link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css'/>
