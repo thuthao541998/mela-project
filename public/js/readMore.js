f3bd097073308785e6223011a7072c59be432348
Thao <thuthao541998@gmail.com>
2018-08-16 22:57:25 +0700

    sửa lại file js 1 chút
    xóa biến dấu 3 chấm đi và chỗ showChar k có -1 nữa

--- public/js/readMore.js
+++ public/js/readMore.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
     var showChar = 150;
-    var ellipsestext = "...";
-    var moretext = "Read More";
-    var lesstext = "Read Less";
+    var moretext = "...Read More";
+    var lesstext = "...Read Less";
     $('.more').each(function() {
         var content = $(this).html();
 
         if(content.length > showChar) {
 
             var c = content.substr(0, showChar);
-            var h = content.substr(showChar-1, content.length - showChar);
+            var h = content.substr(showChar, content.length - showChar);
 
-            var html = c + '<span class="moreelipses">'+ellipsestext+'</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">'+moretext+'</a></span>';
+            var html = c + '<span class="moreelipses"></span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">'+moretext+'</a></span>';
 
             $(this).html(html);
         }
