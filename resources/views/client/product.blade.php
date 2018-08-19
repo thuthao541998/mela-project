62679e72df43f938ce62f962bc5ae2c04d97f136
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-08-19 22:53:46 +0700

    Sửa hiển thị từng product trang list-product

--- resources/views/client/product.blade.php
+++ resources/views/client/product.blade.php
@@ -9,8 +9,33 @@
     <!-- Main menu -->
     <section class="section-mainmenu p-t-110 p-b-70 bg1-pattern">
         <div class="container">
-            <div class="row">
+            <div class="row p-t-108 p-b-70">
                 <div class="col-md-10 col-lg-6 p-r-35 p-r-15-lg m-l-r-auto">
+
+
+                    <div class="blo3 flex-w flex-col-l-sm m-b-30">
+                        <div class="pic-blo3 size20 bo-rad-10 hov-img-zoom m-r-28">
+                            <a href="#"><img src="images/lunch-01.jpg" alt="IMG-MENU"></a>
+                        </div>
+
+                        <div class="text-blo3 size21 flex-col-l-m">
+                            <a href="#" class="txt21 m-b-3">
+                                Sed varius
+                            </a>
+
+                            <span class="txt23">
+								Aenean pharetra tortor dui in pellentesque
+							</span>
+
+                            <span class="txt22 m-t-20">
+								$29.79
+							</span>
+                        </div>
+                    </div>
+
+
+
+
                     <div class="wrap-item-mainmenu p-b-22">
                         <div class="tit-mainmenu tit10 p-b-25">
                             <form>
@@ -27,293 +52,51 @@
 
                         @foreach($list_obj as $obj)
 
+
+                            <div class="blo3 flex-w flex-col-l-sm m-b-30">
+                                <div class="pic-blo3 size20 bo-rad-10 hov-img-zoom m-r-28">
+                                    <a href="#"><img src="{{$obj->images}}" alt="IMG-MENU"></a>
+                                </div>
+
+                                <div class="text-blo3 size21 flex-col-l-m">
+                                    <a href="#" class="txt21 m-b-3">
+                                        {{$obj->name}}
+                                    </a>
+
+                                    <span class="txt23">
+                                        {{$obj->overview}}
+                                    </span>
+
+                                    <span class="txt22 m-t-20">
+                                        {{$obj->price}}
+                                    </span>
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+                            <div class="item-mainmenu m-b-36">
+                                <div class="flex-w flex-b m-b-3">
+                                    <a href="#" class="name-item-mainmenu txt21">
+                                        Pine nut sbrisalona
+                                    </a>
+
+                                    <div class="line-item-mainmenu bg3-pattern"></div>
+
+                                    <div class="price-item-mainmenu txt22">
+                                        $29.79
+                                    </div>
+                                </div>
+
+                                <span class="info-item-mainmenu txt23">
+								    Sed fermentum eros vitae eros
+							    </span>
+                            </div>
                         @endforeach
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Pine nut sbrisalona
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $29.79
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Sed fermentum eros vitae eros
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Aenean eu
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $19.35
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Feugiat maximus neque pharetra
-							</span>
-                        </div>
-
-
-
-                    <div class="wrap-item-mainmenu p-b-22">
-                        <h3 class="tit-mainmenu tit10 p-b-25">
-                            Drinks
-                        </h3>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Vivamus pretium
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $29.79
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Sed fermentum eros vitae eros
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Duis pharetra ligula
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $19.35
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Feugiat maximus neque pharetra
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    In eu dolor
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $53.34
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Proin lacinia nisl ut ultricies posuere nulla
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Feugiat maximus
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $62.45
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Sed fermentum eros vitae eros
-							</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-10 col-lg-6 p-l-35 p-l-15-lg m-l-r-auto">
-                    <div class="wrap-item-mainmenu p-b-22">
-                        <h3 class="tit-mainmenu tit10 p-b-25">
-                            Main
-                        </h3>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Duis sed aliquet
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $31.18
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Proin lacinia nisl ut ultricies posuere nulla
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Suspendisse
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $70.25
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Feugiat maximus neque pharetra
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Scelerisque sed
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $36.19
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Etiam cursus facilisis tortor
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Mollis nulla
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $19.50
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Proin lacinia nisl ut ultricies posuere nulla
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Convallis augue
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $29.15
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Sed fermentum eros vitae eros
-							</span>
-                        </div>
-
-
-                    <div class="wrap-item-mainmenu p-b-22">
-                        <h3 class="tit-mainmenu tit10 p-b-25">
-                            Dessert
-                        </h3>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    tempus aliquet
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $9.79
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Proin lacinia nisl ut ultricies posuere nulla
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    scelerisque
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $19.35
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Sed fermentum eros vitae eros
-							</span>
-                        </div>
-
-                        <!-- Item mainmenu -->
-                        <div class="item-mainmenu m-b-36">
-                            <div class="flex-w flex-b m-b-3">
-                                <a href="#" class="name-item-mainmenu txt21">
-                                    Cras maximus
-                                </a>
-
-                                <div class="line-item-mainmenu bg3-pattern"></div>
-
-                                <div class="price-item-mainmenu txt22">
-                                    $5.79
-                                </div>
-                            </div>
-
-                            <span class="info-item-mainmenu txt23">
-								Duis pharetra ligula at urna dignissim
-							</span>
-                        </div>
                     </div>
                 </div>
             </div>
