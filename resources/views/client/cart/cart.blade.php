3f74d30a0a16cb260d410e4b3a8022391c1bc348
Tra <trauvmd00621@fpt.edu.vn>
2018-08-23 16:29:34 +0700

    Sửa nốt ảnh và button

--- resources/views/client/cart/cart.blade.php
+++ resources/views/client/cart/cart.blade.php
@@ -23,51 +23,42 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td class="col-sm-8 col-md-6">
-                                <div class="media">
-                                    <a class="thumbnail pull-left" href="#"> <img class="media-object" src="{{asset('/images/photo-gallery-thumb-13.jpg')}}{{--{{$obj->images}}--}}" style="width: 72px; height: auto;"> </a>
-                                    <div class="media-body">
-                                        <h4 class="media-heading"><a href="#">Product name</a></h4>
-                                        <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
-                                        <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
-                                    </div>
-                                </div></td>
-                            <td class="col-sm-1 col-md-1" style="text-align: center">
-                                <input type="email" class="form-control" id="exampleInputEmail1" value="3">
-                            </td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
-                            <td class="col-sm-1 col-md-1">
-                                <button type="button" class="btn btn-danger">
-                                    <span class="glyphicon glyphicon-remove"></span> Remove
-                                </button></td>
-                        </tr>
+                        {{--@foreach($list_obj as $obj)--}}
+                            {{--<span class="hidden" {{$brand = $brands->find($obj->brandId)}}></span>--}}
                         <tr>
                             <td class="col-md-6">
                                 <div class="media">
-                                    <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
-                                    <div class="media-body">
-                                        <h4 class="media-heading"><a href="#">Product name</a></h4>
-                                        <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
-                                        <span>Status: </span><span class="text-warning"><strong>Leaves warehouse in 2 - 3 weeks</strong></span>
+                                    <div class="card thumbnail pull-left"
+                                         style="background-image: url('{{asset('/images/photo-gallery-thumb-12.jpg')}}{{--{{$obj->images}}--}}'); background-size: cover; width: 60px; height: 60px; margin-right: 20px;">
+                                    </div>
+                                    <div class="media-body" style="margin-left: 50px;">
+                                        <h4 class="media-heading">{{--{{$obj->name}}--}}Product name</h4>
+                                        {{--chuyển categoryId của product thành category--}}
+                                        <h5 class="media-heading"> {{--{{$brand->name}}--}}Brand name</h5>
+                                        {{--xử lí status của product từ số thành chữ--}}
+                                        <span>Status: </span><span class="text-warning"><strong>{{--{{$obj->status}}--}}Leaves warehouse in 2 - 3 weeks</strong></span>
                                     </div>
                                 </div></td>
-                            <td class="col-md-1" style="text-align: center">
-                                <input type="email" class="form-control" id="exampleInputEmail1" value="2">
+                            <td class="col-md-1" style="text-align: center;">
+                                <input type="number" class="form-control" id="exampleInputEmail1" value="2">
+                                {{--lấy số lượng sản phẩm ở đây--}}
+                                {{--{{$quantity = Input::get(quantity)}}--}}
                             </td>
-                            <td class="col-md-1 text-center"><strong>$4.99</strong></td>
-                            <td class="col-md-1 text-center"><strong>$9.98</strong></td>
+                            <td class="col-md-1 text-center"><strong>{{--{{$unitPrice = $obj->discountPrice}}--}}$4.99</strong></td>
+                            <td class="col-md-1 text-center"><strong>{{--{{$unitPrice*$quantity}}--}}$9.98</strong></td>
                             <td class="col-md-1">
+                                {{--xoá sản phẩm ở đây--}}
                                 <button type="button" class="btn btn-danger">
-                                    <span class="glyphicon glyphicon-remove"></span> Remove
+                                    <span class="fa fa-trash"></span> {{----}}Remove
                                 </button></td>
                         </tr>
+                        {{--@endforeach--}}
                         <tr>
                             <td>   </td>
                             <td>   </td>
                             <td>   </td>
                             <td><h5>Subtotal</h5></td>
+                            {{--tổng tiền các sản phẩm ở đây--}}
                             <td class="text-right"><h5><strong>$24.59</strong></h5></td>
                         </tr>
                         <tr>
@@ -75,6 +66,7 @@
                             <td>   </td>
                             <td>   </td>
                             <td><h5>Estimated shipping</h5></td>
+                            {{--giá ship ở đây, kệ mẹ nó--}}
                             <td class="text-right"><h5><strong>$6.94</strong></h5></td>
                         </tr>
                         <tr>
@@ -82,6 +74,7 @@
                             <td>   </td>
                             <td>   </td>
                             <td><h3>Total</h3></td>
+                            {{--tổng tiền phải trả ở đây, giá ship + tổng tiền sản phẩm--}}
                             <td class="text-right"><h3><strong>$31.53</strong></h3></td>
                         </tr>
                         <tr>
@@ -90,11 +83,12 @@
                             <td>   </td>
                             <td>
                                 <button type="button" class="btn btn-default">
-                                    <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
+                                    <span class="fa fa-shopping-cart"></span> Continue Shopping
                                 </button></td>
                             <td>
+                                {{--xử lí thanh toán ở đây--}}
                                 <button type="button" class="btn btn-success">
-                                    Checkout <span class="glyphicon glyphicon-play"></span>
+                                    Checkout <span class="fa fa-play"></span>
                                 </button></td>
                         </tr>
                         </tbody>
