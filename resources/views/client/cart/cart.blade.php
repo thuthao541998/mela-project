aa1b797c04705a568528d2d2f6fb9167660c17a1
Tra <trauvmd00621@fpt.edu.vn>
2018-08-23 20:13:45 +0700

    DAC cart

--- resources/views/client/cart/cart.blade.php
+++ resources/views/client/cart/cart.blade.php
@@ -2,6 +2,17 @@
 @section('content')
     <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Montserrat:300' rel='stylesheet' type='text/css'>
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset('images/icons/favicon.png')}}"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/select2.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/cart.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
     <div style="background-color: white;">
         <div class="container">
             <div class="row" style="margin: 50px;">
@@ -10,89 +21,168 @@
                 </div>
             </div>
             </hr>
-            <div class="row">
-                <div class="col-sm-12 col-md-10 col-md-offset-1">
-                    <table class="table table-hover">
-                        <thead>
-                        <tr style="font-family: 'Montserrat', sans-serif; text-transform: uppercase;">
-                            <th>Product</th>
-                            <th>Quantity</th>
-                            <th class="text-center">Price</th>
-                            <th class="text-center">Total</th>
-                            <th> </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {{--@foreach($list_obj as $obj)--}}
-                            {{--<span class="hidden" {{$brand = $brands->find($obj->brandId)}}></span>--}}
-                        <tr>
-                            <td class="col-md-6">
-                                <div class="media">
-                                    <div class="card thumbnail pull-left"
-                                         style="background-image: url('{{asset('/images/photo-gallery-thumb-12.jpg')}}{{--{{$obj->images}}--}}'); background-size: cover; width: 60px; height: 60px; margin-right: 20px;">
+
+            <div class="container">
+                <!-- Cart item -->
+                <div class="container-table-cart pos-relative">
+                    <div class="wrap-table-shopping-cart bgwhite">
+                        <table class="table-shopping-cart">
+                            <tbody><tr class="table-head">
+                                <th class="column-1"></th>
+                                <th class="column-2">Product</th>
+                                <th class="column-3">Price</th>
+                                <th class="column-4 p-l-70">Quantity</th>
+                                <th class="column-5">Total</th>
+                            </tr>
+
+                            <tr class="table-row">
+                                <td class="column-1">
+                                    <div class="cart-img-product b-rad-4 o-f-hidden">
+                                        <img src="images/item-10.jpg" alt="IMG-PRODUCT">
                                     </div>
-                                    <div class="media-body" style="margin-left: 50px;">
-                                        <h4 class="media-heading">{{--{{$obj->name}}--}}Product name</h4>
-                                        {{--chuyển categoryId của product thành category--}}
-                                        <h5 class="media-heading"> {{--{{$brand->name}}--}}Brand name</h5>
-                                        {{--xử lí status của product từ số thành chữ--}}
-                                        <span>Status: </span><span class="text-warning"><strong>{{--{{$obj->status}}--}}Leaves warehouse in 2 - 3 weeks</strong></span>
+                                </td>
+                                <td class="column-2">Men Tshirt</td>
+                                <td class="column-3">$36.00</td>
+                                <td class="column-4">
+                                    <div class="flex-w bo5 of-hidden w-size17">
+                                        <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+                                            <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+                                        </button>
+
+                                        <input class="size8 m-text18 t-center num-product" name="num-product1" value="1" type="number">
+
+                                        <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+                                            <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+                                        </button>
                                     </div>
-                                </div></td>
-                            <td class="col-md-1" style="text-align: center;">
-                                <input type="number" class="form-control" id="exampleInputEmail1" value="2">
-                                {{--lấy số lượng sản phẩm ở đây--}}
-                                {{--{{$quantity = Input::get(quantity)}}--}}
-                            </td>
-                            <td class="col-md-1 text-center"><strong>{{--{{$unitPrice = $obj->discountPrice}}--}}$4.99</strong></td>
-                            <td class="col-md-1 text-center"><strong>{{--{{$unitPrice*$quantity}}--}}$9.98</strong></td>
-                            <td class="col-md-1">
-                                {{--xoá sản phẩm ở đây--}}
-                                <button type="button" class="btn btn-danger">
-                                    <span class="fa fa-trash"></span> {{----}}Remove
-                                </button></td>
-                        </tr>
-                        {{--@endforeach--}}
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td><h5>Subtotal</h5></td>
-                            {{--tổng tiền các sản phẩm ở đây--}}
-                            <td class="text-right"><h5><strong>$24.59</strong></h5></td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td><h5>Estimated shipping</h5></td>
-                            {{--giá ship ở đây, kệ mẹ nó--}}
-                            <td class="text-right"><h5><strong>$6.94</strong></h5></td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td><h3>Total</h3></td>
-                            {{--tổng tiền phải trả ở đây, giá ship + tổng tiền sản phẩm--}}
-                            <td class="text-right"><h3><strong>$31.53</strong></h3></td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>
-                                <button type="button" class="btn btn-default">
-                                    <span class="fa fa-shopping-cart"></span> Continue Shopping
-                                </button></td>
-                            <td>
-                                {{--xử lí thanh toán ở đây--}}
-                                <button type="button" class="btn btn-success">
-                                    Checkout <span class="fa fa-play"></span>
-                                </button></td>
-                        </tr>
-                        </tbody>
-                    </table>
+                                </td>
+                                <td class="column-5">$36.00</td>
+                            </tr>
+
+                            <tr class="table-row">
+                                <td class="column-1">
+                                    <div class="cart-img-product b-rad-4 o-f-hidden">
+                                        <img src="images/item-05.jpg" alt="IMG-PRODUCT">
+                                    </div>
+                                </td>
+                                <td class="column-2">Mug Adventure</td>
+                                <td class="column-3">$16.00</td>
+                                <td class="column-4">
+                                    <div class="flex-w bo5 of-hidden w-size17">
+                                        <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+                                            <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+                                        </button>
+
+                                        <input class="size8 m-text18 t-center num-product" name="num-product2" value="1" type="number">
+
+                                        <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+                                            <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="column-5">$16.00</td>
+                            </tr>
+                            </tbody></table>
+                    </div>
+                </div>
+
+                <div class="flex-w flex-sb-m p-t-25 p-b-25 bo8 p-l-35 p-r-60 p-lr-15-sm">
+                    <div class="flex-w flex-m w-full-sm">
+                        <div class="size11 bo4 m-r-10">
+                            <input class="sizefull s-text7 p-l-22 p-r-22" name="coupon-code" placeholder="Coupon Code" type="text">
+                        </div>
+
+                        <div class="size12 trans-0-4 m-t-10 m-b-10 m-r-10">
+                            <!-- Button -->
+                            <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
+                                Apply coupon
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="size10 trans-0-4 m-t-10 m-b-10">
+                        <!-- Button -->
+                        <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
+                            Update Cart
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Total -->
+                <div class="bo9 w-size18 p-l-40 p-r-40 p-t-30 p-b-38 m-t-30 m-r-0 m-l-auto p-lr-15-sm">
+                    <h5 class="m-text20 p-b-24">
+                        Cart Totals
+                    </h5>
+
+                    <!--  -->
+                    <div class="flex-w flex-sb-m p-b-12">
+					<span class="s-text18 w-size19 w-full-sm">
+						Subtotal:
+					</span>
+
+                        <span class="m-text21 w-size20 w-full-sm">
+						$39.00
+					</span>
+                    </div>
+
+                    <!--  -->
+                    <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
+					<span class="s-text18 w-size19 w-full-sm">
+						Shipping:
+					</span>
+
+                        <div class="w-size20 w-full-sm">
+                            <p class="s-text8 p-b-23">
+                                There are no shipping methods available. Please double check your address, or contact us if you need any help.
+                            </p>
+
+                            <span class="s-text19">
+							Calculate Shipping
+						</span>
+
+                            <div class="rs2-select2 rs3-select2 rs4-select2 bo4 of-hidden w-size21 m-t-8 m-b-12">
+                                <select class="selection-2 select2-hidden-accessible" name="country" tabindex="-1" aria-hidden="true">
+                                    <option>Select a country...</option>
+                                    <option>US</option>
+                                    <option>UK</option>
+                                    <option>Japan</option>
+                                </select><span class="select2 select2-container select2-container--default" dir="ltr" style="width: 159.883px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-country-lg-container"><span class="select2-selection__rendered" id="select2-country-lg-container" title="Select a country...">Select a country...</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+                            </div>
+
+                            <div class="size13 bo4 m-b-12">
+                                <input class="sizefull s-text7 p-l-15 p-r-15" name="state" placeholder="State /  country" type="text">
+                            </div>
+
+                            <div class="size13 bo4 m-b-22">
+                                <input class="sizefull s-text7 p-l-15 p-r-15" name="postcode" placeholder="Postcode / Zip" type="text">
+                            </div>
+
+                            <div class="size14 trans-0-4 m-b-10">
+                                <!-- Button -->
+                                <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
+                                    Update Totals
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--  -->
+                    <div class="flex-w flex-sb-m p-t-26 p-b-30">
+					<span class="m-text22 w-size19 w-full-sm">
+						Total:
+					</span>
+
+                        <span class="m-text21 w-size20 w-full-sm">
+						$39.00
+					</span>
+                    </div>
+
+                    <div class="size15 trans-0-4">
+                        <!-- Button -->
+                        <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
+                            Proceed to Checkout
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
