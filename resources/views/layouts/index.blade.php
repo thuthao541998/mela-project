aff878b54f6c4df0cde05517512e059e6ba73f67
Thao <thuthao541998@gmail.com>
2018-08-25 00:14:52 +0700

    Revert "update form chuẩn"
    This reverts commit 23e7e23c1485be09595acded8166888b13879b87.

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -30,7 +30,7 @@
         <!--===============================================================================================-->
         <link rel="stylesheet" type="text/css" href="{{asset('css/lightbox.min.css')}}">
         <!--===============================================================================================-->
-        {{--<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">--}}
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
         <!--===============================================================================================-->
         <link rel="stylesheet" type="text/css" href="{{asset('css/util.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
