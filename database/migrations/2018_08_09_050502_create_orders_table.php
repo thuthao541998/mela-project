666d9fc5c58ac94ffbab2e649ee6b276b180a892
nnt2612 <33348945+nnt2612@users.noreply.github.com>
2018-08-24 20:09:59 +0700

    Sửa các trường trong bảng order và order detail giống thầy

--- database/migrations/2018_08_09_050502_create_orders_table.php
+++ database/migrations/2018_08_09_050502_create_orders_table.php
@@ -16,9 +16,13 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps(true);
-            $table->integer('total');
+            $table->integer('customer_id');
+            $table->integer('total_price');
+            $table->string('ship_name');
+            $table->string('ship_address');
+            $table->string('ship_phone');
             $table->integer('clientId');
-            $table->integer('status')->default(1);
+            $table->integer('status')->default(1); // -1. huỷ | 0. đang chờ xử lý | 1. đã nhận đơn hàng | 2. hoàn thành.
         });
     }
 
