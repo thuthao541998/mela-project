c36d38a286c5523b01d822fac0e5f909921b0693
nh0ckjapan <31547509+nh0ckjapan@users.noreply.github.com>
2018-08-25 13:56:01 +0700

    update index page
    update index page

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -553,24 +553,26 @@
 
 
     <!-- Video -->
-    <section class="section-video parallax100"
-             style="background-image: url({{asset('images/bg-cover-video-02.jpg')}});">
-        <div class="content-video t-center p-t-225 p-b-250">
-            <span class="tit2 p-l-15 p-r-15">
-                Discover
-            </span>
+    {{--<section class="section-video parallax100"--}}
+             {{--style="background-image: url({{asset('images/bg-cover-video-02.jpg')}});">--}}
+        {{--<div class="content-video t-center p-t-225 p-b-250">--}}
+            {{--<span class="tit2 p-l-15 p-r-15">--}}
+                {{--Discover--}}
+            {{--</span>--}}
 
-            <h3 class="tit4 t-center p-l-15 p-r-15 p-t-3">
-                Our Video
-            </h3>
+            {{--<h3 class="tit4 t-center p-l-15 p-r-15 p-t-3">--}}
+                {{--Our Video--}}
+            {{--</h3>--}}
 
-            <div class="btn-play ab-center size16 hov-pointer m-l-r-auto m-t-43 m-b-33" data-toggle="modal"
-                 data-target="#modal-video-01">
-                <div class="flex-c-m sizefull bo-cir bgwhite color1 hov1 trans-0-4">
-                    <i data-video="https://www.youtube.com/embed/HBdcL7YsdBY" class="fa fa-play fs-18 m-l-2" aria-hidden="true"></i>
-                </div>
-            </div>
-        </div>
-    </section>
+            {{--<div class="btn-play ab-center size16 hov-pointer m-l-r-auto m-t-43 m-b-33" data-toggle="modal"--}}
+                 {{--data-target="#modal-video-01">--}}
+                {{--<div class="flex-c-m sizefull bo-cir bgwhite color1 hov1 trans-0-4">--}}
+                    {{--<i class="fa fa-play fs-18 m-l-2" aria-hidden="true"></i>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
+
+
 
 @endsection
