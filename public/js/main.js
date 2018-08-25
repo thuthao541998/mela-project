d230d0ec61f5a96a369488e45bb603cd67b39607
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-08-26 01:01:08 +0700

    Hiển thị modal cart trên client layout index

--- public/js/main.js
+++ public/js/main.js
@@ -192,6 +192,9 @@
         });
     });
 
+
+
+
     /*[ Show menu mobile ]
     ===========================================================*/
     $('.btn-show-menu-mobile').on('click', function(){
@@ -227,6 +230,42 @@
     $('.btn-romove-top-noti').on('click', function(){
         $(this).parent().remove();
     })
+
+    $('.js-show-header-dropdown').on('click', function(){
+        $(this).parent().find('.header-dropdown')
+    });
+
+    var menu = $('.js-show-header-dropdown');
+    var sub_menu_is_showed = -1;
+
+    for(var i=0; i<menu.length; i++){
+        $(menu[i]).on('click', function(){
+
+            if(jQuery.inArray( this, menu ) == sub_menu_is_showed){
+                $(this).parent().find('.header-dropdown').toggleClass('show-header-dropdown');
+                sub_menu_is_showed = -1;
+            }
+            else {
+                for (var i = 0; i < menu.length; i++) {
+                    $(menu[i]).parent().find('.header-dropdown').removeClass("show-header-dropdown");
+                }
+
+                $(this).parent().find('.header-dropdown').toggleClass('show-header-dropdown');
+                sub_menu_is_showed = jQuery.inArray( this, menu );
+            }
+        });
+    }
+
+    $(".js-show-header-dropdown, .header-dropdown").click(function(event){
+        event.stopPropagation();
+    });
+
+    $(window).on("click", function(){
+        for (var i = 0; i < menu.length; i++) {
+            $(menu[i]).parent().find('.header-dropdown').removeClass("show-header-dropdown");
+        }
+        sub_menu_is_showed = -1;
+    });
 
 
 
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -89,10 +89,153 @@
                 <div class="social flex-w flex-l-m p-r-20">
                     <button class="btn-show-sidebar m-l-33 trans-0-4"></button>
                     <span class="p-l-30">
-                        <a href="/cart" class="fa fa-shopping-cart fa-2x"></a>
-                        <span class="header-icons-noti m-l-30">0</span>
+                        <div class="btn-show-menu">
+
+
+
+
+                            <div class="wrap_header_mobile" style="background-color: transparent; color: #fff;">
+                                
+
+                                <!-- Button show menu -->
+                                <div class="btn-show-menu">
+                                    <!-- Header Icon mobile -->
+                                    <div class="header-icons-mobile">
+                                        <div class="header-wrapicon2">
+                                            {{--<img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">--}}
+                                            {{--<span class="header-icons-noti">0</span>--}}
+
+
+                                            <a href="#" class="fa fa-shopping-cart fa-2x js-show-header-dropdown"></a>
+                                    <span class="header-icons-noti m-l-30" id="header-icons-noti">{{\App\Cart::getTotalItem()}}</span>
+
+                                            <!-- Header cart noti -->
+                                            <div class="header-cart header-dropdown">
+                                                <ul class="header-cart-wrapitem">
+                                                    <li class="header-cart-item">
+                                                        <div class="header-cart-item-img">
+                                                            <img src="images/item-cart-01.jpg" alt="IMG">
+                                                        </div>
+
+                                                        <div class="header-cart-item-txt">
+                                                            <a href="#" class="header-cart-item-name">
+                                                                White Shirt With Pleat Detail Back
+                                                            </a>
+
+                                                            <span class="header-cart-item-info">
+                                                        1 x $19.00
+                                                    </span>
+                                                        </div>
+                                                    </li>
+
+                                                    <li class="header-cart-item">
+                                                        <div class="header-cart-item-img">
+                                                            <img src="images/item-cart-02.jpg" alt="IMG">
+                                                        </div>
+
+                                                        <div class="header-cart-item-txt">
+                                                            <a href="#" class="header-cart-item-name">
+                                                                Converse All Star Hi Black Canvas
+                                                            </a>
+
+                                                            <span class="header-cart-item-info">
+                                                        1 x $39.00
+                                                    </span>
+                                                        </div>
+                                                    </li>
+
+                                                    <li class="header-cart-item">
+                                                        <div class="header-cart-item-img">
+                                                            <img src="images/item-cart-03.jpg" alt="IMG">
+                                                        </div>
+
+                                                        <div class="header-cart-item-txt">
+                                                            <a href="#" class="header-cart-item-name">
+                                                                Nixon Porter Leather Watch In Tan
+                                                            </a>
+
+                                                            <span class="header-cart-item-info">
+                                                        1 x $17.00
+                                                    </span>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+
+                                                <div class="header-cart-total">
+                                                    Total: $75.00
+                                                </div>
+
+                                                <div class="header-cart-buttons">
+                                                    <div class="header-cart-wrapbtn">
+                                                        <!-- Button -->
+                                                        <a href="cart.html" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                                            View Cart
+                                                        </a>
+                                                    </div>
+
+                                                    <div class="header-cart-wrapbtn">
+                                                        <!-- Button -->
+                                                        <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                                            Check Out
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+                                        <span class="hamburger-box">
+                                            <span class="hamburger-inner"></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        {{--<div class="header-cart header-dropdown">--}}
+                            {{--<ul class="header-cart-wrapitem" id="header-cart-wrapitem">--}}
+                                {{--@if(count(\App\Cart::getCart()->items)>0)--}}
+                                    {{--@foreach(\App\Cart::getCart()->items as $item)--}}
+                                        {{--<li class="header-cart-item">--}}
+                                        {{--<div class="header-cart-item-img">--}}
+                                            {{--<img src="{{$item->product->images}}" alt="IMG">--}}
+                                        {{--</div>--}}
+
+                                        {{--<div class="header-cart-item-txt">--}}
+                                            {{--<a href="#" class="header-cart-item-name">--}}
+                                                {{--{{$item->product->name}}--}}
+                                            {{--</a>--}}
+
+                                            {{--<span class="header-cart-item-info">--}}
+                                                    {{--{{$item->quantity}} x {{$item->product->discountPriceString}}--}}
+                                                {{--</span>--}}
+                                        {{--</div>--}}
+                                    {{--</li>--}}
+                                    {{--@endforeach--}}
+                                {{--@else--}}
+                                    {{--'Hiện tại không có sản phẩm nào trong giỏ hàng'--}}
+                                {{--@endif--}}
+                            {{--</ul>--}}
+                            {{--<div class="header-cart-total">--}}
+                                {{--Tổng cộng: <span id="header-cart-total">{{\App\Cart::getCart()->getTotalMoneyString()}}</span>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="header-cart-buttons">--}}
+                                {{--<div class="header-cart-wrapbtn"></div>--}}
+                                {{--<div class="header-cart-wrapbtn">--}}
+                                    {{--<!-- Button -->--}}
+                                    {{--<a href="/cart" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">--}}
+                                        {{--Đơn hàng--}}
+                                    {{--</a>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        </div>
                     </span>
                 </div>
+
+
             </div>
         </div>
     </div>
