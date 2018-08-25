e926e7a0e6773f99d0fe8006bbb43d93a00972dc
Thao <thuthao541998@gmail.com>
2018-08-25 13:54:43 +0700

    seeder auto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(ProductTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
+
     }
 }
