d920a2e62132eb768814b74dc000661146fd11eb
Thao <thuthao541998@gmail.com>
2018-08-26 12:05:05 +0700

    table

--- database/migrations/2018_08_19_043807_create_order_details_table.php
+++ database/migrations/2018_08_19_043807_create_order_details_table.php
@@ -13,14 +13,17 @@ class CreateOrderDetailsTable extends Migration
      */
     public function up()
     {
+
         Schema::create('order_details', function (Blueprint $table) {
-            $table->integer('order_id');
-            $table->integer('product_id');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders');;
             $table->primary(['order_id', 'product_id']);
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->integer('quantity');
             $table->integer('unit_price');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
