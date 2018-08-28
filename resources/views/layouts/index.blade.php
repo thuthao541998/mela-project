4d7c67ae1d1c59eb1523910f79f29cb0c24e9e58
Thao <thuthao541998@gmail.com>
2018-08-28 18:44:49 +0700

    sửa blog

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -317,105 +317,106 @@
 
 <!-- Blog -->
 <section class="section-blog bg-white p-t-115 p-b-123">
-    <div class="container">
-        <div class="title-section-ourmenu t-center m-b-22">
+        <div class="container">
+            <div class="title-section-ourmenu t-center m-b-22">
                 <span class="tit2 t-center">
                     Latest News
                 </span>
 
-            <h3 class="tit5 t-center m-t-2">
-                The Blog
-            </h3>
+                <h3 class="tit5 t-center m-t-2">
+                    The Blog
+                </h3>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 p-t-30">
+                    <!-- Block1 -->
+                    <div class="blo1">
+                        <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
+                            <a href="blog-detail.html"><img src="images/blog-01.jpg" alt="IMG-INTRO"></a>
+
+                            <div class="time-blog">
+                                21 Dec 2017
+                            </div>
+                        </div>
+
+                        <div class="wrap-text-blo1 p-t-35">
+                            <a href="blog-detail.html"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
+                                    Not only is beauty
+                                </h4></a>
+
+                            <p class="m-b-20">
+                                Mela shop with lipstick is not just a normal lipstick beauty, but also ...
+                            </p>
+
+                            <a href="blog-detail.html" class="txt4">
+                                Continue Reading
+                                <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 p-t-30">
+                    <!-- Block1 -->
+                    <div class="blo1">
+                        <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
+                            <a href="blog-detail.html"><img src="images/blog-02.jpg" alt="IMG-INTRO"></a>
+
+                            <div class="time-blog">
+                                15 Dec 2017
+                            </div>
+                        </div>
+
+                        <div class="wrap-text-blo1 p-t-35">
+                            <a href="blog-detail.html"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
+                                    Darkness
+                                </h4></a>
+
+                            <p class="m-b-20">
+                                Mela lipstick always makes you stand out Shop though not enough light, by ...
+                            </p>
+
+                            <a href="blog-detail.html" class="txt4">
+                                Continue Reading
+                                <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 p-t-30">
+                    <!-- Block1 -->
+                    <div class="blo1">
+                        <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
+                            <a href="blog-detail.html"><img src="images/blog-03.jpg" alt="IMG-INTRO"></a>
+
+                            <div class="time-blog">
+                                12 Dec 2017
+                            </div>
+                        </div>
+
+                        <div class="wrap-text-blo1 p-t-35">
+                            <a href="blog-detail.html"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
+                                    Daily life
+                                </h4></a>
+
+                            <p class="m-b-20">
+                                Mela Shop with the concept new lipstick model nice, but just ...
+                            </p>
+
+                            <a href="blog-detail.html" class="txt4">
+                                Continue Reading
+                                <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+    </section>
 
-        <div class="row">
-            <div class="col-md-4 p-t-30">
-                <!-- Block1 -->
-                <div class="blo1">
-                    <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
-                        <a href="blog-detail.html"><img src="images/blog-01.jpg" alt="IMG-INTRO"></a>
-
-                        <div class="time-blog">
-                            21 Dec 2017
-                        </div>
-                    </div>
-
-                    <div class="wrap-text-blo1 p-t-35">
-                        <a href="/list-artcle"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
-                                Best Places for Wine
-                            </h4></a>
-
-                        <p class="m-b-20">
-                            Phasellus lorem enim, luctus ut velit eget, con-vallis egestas eros.
-                        </p>
-
-                        <a href="/list-artcle" class="txt4">
-                            Continue Reading
-                            <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 p-t-30">
-                <!-- Block1 -->
-                <div class="blo1">
-                    <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
-                        <a href="/list-artcle"><img src="images/blog-02.jpg" alt="IMG-INTRO"></a>
-
-                        <div class="time-blog">
-                            15 Dec 2017
-                        </div>
-                    </div>
-
-                    <div class="wrap-text-blo1 p-t-35">
-                        <a href="/list-artcle"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
-                                Eggs and Cheese
-                            </h4></a>
-
-                        <p class="m-b-20">
-                            Duis elementum, risus sit amet lobortis nunc justo condimentum ligula, vitae feugiat
-                        </p>
-
-                        <a href="/list-artcle" class="txt4">
-                            Continue Reading
-                            <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 p-t-30">
-                <!-- Block1 -->
-                <div class="blo1">
-                    <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
-                        <a href="/list-artcle"><img src="images/blog-03.jpg" alt="IMG-INTRO"></a>
-
-                        <div class="time-blog">
-                            12 Dec 2017
-                        </div>
-                    </div>
-
-                    <div class="wrap-text-blo1 p-t-35">
-                        <a href="/list-artcle"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
-                                Style the Wedding Party
-                            </h4></a>
-
-                        <p class="m-b-20">
-                            Sed ornare ligula eget tortor tempor, quis porta tellus dictum.
-                        </p>
-
-                        <a href="/list-artcle" class="txt4">
-                            Continue Reading
-                            <i class="fa fa-long-arrow-right m-l-10" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
 
 
 <!-- Sign up -->
