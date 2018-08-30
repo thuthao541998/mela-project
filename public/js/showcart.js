393539f128d6bbab1d9923f74ec588af292bcb93
Thao <thuthao541998@gmail.com>
2018-08-30 19:07:02 +0700

    commit

---
+++ public/js/showcart.js
@@ -0,0 +1,102 @@
+$('.btn-num-product-down').click(function () {
+    var oldTotal = $(this).parent().parent().next().text().replace(/\,/g, '');
+    if ($(this).next().val() > 1) {
+        var value = $(this).next().val();
+        $(this).next().val(value -= 1);
+        $(this).parent().parent().next().text($(this).next().val().replace(/\,/g, '') * parseInt($(this).parent().parent().prev().text().replace(/\,/g, ''))).formatNumber();
+        var currentTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
+        var minus = oldTotal - currentTotal;
+        var totalMoney = parseInt($('.total-money').text().replace(/\,/g, ''));
+        $('.total-money').text(totalMoney - minus).formatNumber();
+    }
+
+});
+$('.btn-num-product-up').click(function () {
+    var oldTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
+    var value = parseInt($(this).prev().val());
+    $(this).prev().val(value += 1);
+    $(this).parent().parent().next().text($(this).prev().val().replace(/\,/g, '') * parseInt($(this).parent().parent().prev().text().replace(/\,/g, ''))).formatNumber();
+    var currentTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
+    var plus = currentTotal - oldTotal;
+    var totalMoney = parseInt($('.total-money').text().replace(/\,/g, '').replace(/\,/g, ''));
+    $('.total-money').text(totalMoney + plus).formatNumber();
+});
+$(".cart_delete").click(function () {
+    var id = $(this).children().attr('id').replace('delete-', '');
+    var deleteTotal = parseInt($('#unit-total-' + id).text().replace(/\,/g, ''));
+    var totalMoney = parseInt($('.total-money').text().replace(/\,/g, ''));
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this product",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    method: 'GET',
+                    url: '/cart-remove/' + id,
+                    data: {
+                        '_token': $('meta[name="csrf-token"]').attr('content'),
+                        'id': id
+                    },
+                    success: function (resp) {
+                        $('.total-money').text(totalMoney - deleteTotal).formatNumber();
+                        swal("Poof! This product has been remove!", {
+                            icon: "success",
+                        });
+                        $('#row-' + id).closest('tr').remove();
+                        if ($('tbody').children().length == 1) {
+                            setTimeout(function () {
+                                window.location.reload(1);
+                            }, 1 * 1000);
+                        }
+                        ;
+
+                    },
+                    error: function (r) {
+                        console.log(r)
+                    }
+                });
+            } else {
+                swal("This product is safe!");
+            }
+            ;
+        });
+});
+
+$('.checkout-btn').click(function () {
+    if ($('input[name="ship_name"]').length < 7) {
+        $('span[name="ship_name"]').html('Your name has to be longer than 7.')
+    };
+    if ($('input[name="ship_address"]').length < 10){
+        $('span[name="ship_address"]').html('Your address has to be longer than 7.');
+    };
+    if (isNaN($('input[name="ship_phone"]'))){
+        $('span[name="ship_phone"]').html('This field has to be number');
+    } else if ($('input[name="ship_phone"]').length < 10){
+        $('span[name="ship_phone"]').html('Your phone has to have 10 letters.');
+    };
+});
+$('input[name="save-changes"]').click(function () {
+    var array = [];
+    $('.num-product').each(function (index, item) {
+        array.push($(this).attr('name'));
+    });
+    $.ajax({
+        url: '/sua-gio-hang',
+        method : 'PUT',
+        dataType: 'json',
+        data : {
+            '_token': $('meta[name="csrf-token"]').attr('content'),
+            'products' : array
+        },
+        success: function (resp) {
+            alert('success');
+        },
+        error : function () {
+            alert('failed');
+        }
+    })
+});
--- resources/views/client/cart/cart.blade.php
+++ resources/views/client/cart/cart.blade.php
@@ -50,7 +50,7 @@
                                         <td class="column-4 ml-2">
                                             <div class="flex-w bo5 of-hidden w-size17 quantity"
                                                  style="margin-left: 40px;">
-                                                <button class="btn-num btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+                                                <button type="button" class="btn-num btn-num-product-down color1 flex-c-m size7 bg8 eff2">
                                                     <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
                                                 </button>
 
@@ -58,7 +58,7 @@
                                                        name="products[{{$item->product->id}}]"
                                                        value="{{$item->quantity}}" type="number">
 
-                                                <button class="btn-num btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+                                                <button type="button" class="btn-num btn-num-product-up color1 flex-c-m size7 bg8 eff2">
                                                     <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
                                                 </button>
                                             </div>
@@ -151,93 +151,6 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 {{--<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>--}}
 <script src="{{asset('js/jquery.formatNumber-0.1.1.min.js')}}"></script>
-<script type="text/javascript">
-    $('.btn-num-product-down').click(function () {
-        var oldTotal = $(this).parent().parent().next().text().replace(/\,/g, '');
-        if ($(this).next().val() > 1) {
-            var value = $(this).next().val();
-            $(this).next().val(value -= 1);
-            $(this).parent().parent().next().text($(this).next().val().replace(/\,/g, '') * parseInt($(this).parent().parent().prev().text().replace(/\,/g, ''))).formatNumber();
-            var currentTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
-            var minus = oldTotal - currentTotal;
-            var totalMoney = parseInt($('.total-money').text().replace(/\,/g, ''));
-            $('.total-money').text(totalMoney - minus).formatNumber();
-        }
-
-    });
-    $('.btn-num-product-up').click(function () {
-        var oldTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
-        var value = parseInt($(this).prev().val());
-        $(this).prev().val(value += 1);
-        $(this).parent().parent().next().text($(this).prev().val().replace(/\,/g, '') * parseInt($(this).parent().parent().prev().text().replace(/\,/g, ''))).formatNumber();
-        var currentTotal = parseInt($(this).parent().parent().next().text().replace(/\,/g, ''));
-        var plus = currentTotal - oldTotal;
-        var totalMoney = parseInt($('.total-money').text().replace(/\,/g, '').replace(/\,/g, ''));
-        $('.total-money').text(totalMoney + plus).formatNumber();
-    });
-    $(".cart_delete").click(function () {
-        var id = $(this).children().attr('id').replace('delete-', '');
-        var deleteTotal = parseInt($('#unit-total-' + id).text().replace(/\,/g, ''));
-        var totalMoney = parseInt($('.total-money').text().replace(/\,/g, ''));
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this product",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-            .then((willDelete) => {
-                if (willDelete) {
-                    $.ajax({
-                        method: 'GET',
-                        url: '/cart-remove/' + id,
-                        data: {
-                            '_token': $('meta[name="csrf-token"]').attr('content'),
-                            'id': id
-                        },
-                        success: function (resp) {
-                            $('.total-money').text(totalMoney - deleteTotal).formatNumber();
-                            swal("Poof! This product has been remove!", {
-                                icon: "success",
-                            });
-                            $('#row-' + id).closest('tr').remove();
-                            if ($('tbody').children().length == 1) {
-                                setTimeout(function () {
-                                    window.location.reload(1);
-                                }, 1 * 1000);
-                            }
-                            ;
-
-                        },
-                        error: function (r) {
-                            console.log(r)
-                        }
-                    });
-                } else {
-                    swal("This product is safe!");
-                }
-                ;
-            });
-    });
-
-    $('.checkout-btn').click(function () {
-        if ($('input[name="ship_name"]').length < 7) {
-            $('span[name="ship_name"]').html('Your name has to be longer than 7.')
-        };
-        if ($('input[name="ship_address"]').length < 10){
-            $('span[name="ship_address"]').html('Your address has to be longer than 7.');
-        };
-        if (isNaN($('input[name="ship_phone"]'))){
-            $('span[name="ship_phone"]').html('This field has to be number');
-        } else if ($('input[name="ship_phone"]').length < 10){
-            $('span[name="ship_phone"]').html('Your phone has to have 10 letters.');
-        };
-    });
-    $('input[name="save-changes"]').click(function () {
-        var id = [];
-        id.push($('.quantity input').attr('name'))
-        console.log(id);
-
-    });
+<script type="text/javascript" src="{{asset('js/showcart.js')}}">
 </script>
 @endsection
