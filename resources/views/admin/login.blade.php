c69f8b118a93fd2e163bb9aa344c849e810d9e45
Thao <thuthao541998@gmail.com>
2018-08-31 23:10:09 +0700

    sửa view login của a an

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,12 +1,17 @@
-@extends ('layouts.master')
-@section('page-title', 'Login - Admin Page')
-@section('content')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/admin-login.css')}}">
+    <title>Admin - Login</title>
+</head>
+<body>
+
     <div id="LoginForm">
     <div class="container">
-        <h1 class="form-heading">login Form</h1>
         <div class="login-form">
-            <div class="main-div">
+            <div class="main-div m-t-100">
                 <div class="panel">
                     <h2>Admin Login</h2>
                     <p>Please enter your email and password</p>
@@ -28,13 +33,12 @@
                     <div class="forgot">
                         <a href="reset.html">Forgot password?</a>
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button type="submit" class="btn btn-primary form-control">Login</button>
 
                 </form>
             </div>
-            <p class="botto-text"> Designed by Sunil Rajput</p>
         </div></div></div>
-
-
     </div>
-@endsection
+</body>
+
+</html>
