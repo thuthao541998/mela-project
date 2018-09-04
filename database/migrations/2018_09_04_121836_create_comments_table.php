e27db45f8d506a4a28ccf1ac64556c384ddcbc98
Thao <thuthao541998@gmail.com>
2018-09-04 20:19:59 +0700

    cho vào migration

--- database/migrations/2018_09_04_121836_create_comments_table.php
+++ database/migrations/2018_09_04_121836_create_comments_table.php
@@ -15,7 +15,13 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->string('content',3000);
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
