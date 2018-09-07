b2e499dbc728cb2074ada5ada04e1c8cdfbdf9ae
nguyenhoang24695 <hoangnvd00536@fpt.edu.vn>
2018-09-08 03:35:30 +0700

    test timestamp

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,9 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->integer('coin')->default(1);
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp();
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
