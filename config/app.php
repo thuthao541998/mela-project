a3b0bc1e365b1335bb12ec002cacaf1cf48503f2
Nguyen Hoang <hoangnvd00536@fpt.edu.vn>
2018-09-09 00:36:03 +0700

    require unisharp

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
         'JD\Cloudder\CloudderServiceProvider'
 
     ],
