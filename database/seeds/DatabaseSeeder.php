d4ccb67c883e5ad97b058c688bfdfe95df0ec516
Nguyen Hoang <hoangnvd00536@fpt.edu.vn>
2018-09-09 00:11:51 +0700

    change position seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,14 +14,14 @@ class DatabaseSeeder extends Seeder
 
 
         // $this->call(UsersTableSeeder::class);
-
+        $this->call(SellUserTableSeeder::class);
         $this->call(ProductTableSeeder::class);
         $this->call(ArticlesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(BrandsTableSeeder::class);
         $this->call(OrderDetailsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
-        $this->call(SellUserTableSeeder::class);
+
 
     }
 }
