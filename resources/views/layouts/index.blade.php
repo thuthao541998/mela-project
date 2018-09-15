24f42538a106cdf6a315a17931488df0024c9ec4
Thao <thuthao541998@gmail.com>
2018-09-15 13:29:57 +0700

    sửa view lần cuối (maybe)

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -425,17 +425,16 @@
                     <div class="col-md-4 p-t-30">
                         <!-- Block1 -->
                         <div class="blo1">
-                            <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">                                        The New Lipstick Range Designed to Flatter Asian Skin
-
+                            <div class="wrap-pic-blo1 bo-rad-10 hov-img-zoom pos-relative">
                                 <a href="/article/8"><img src="{{asset('images/blog-03.jpg')}}" alt="IMG-INTRO"></a>
                             </div>
 
                             <div class="wrap-text-blo1 p-t-35">
                                 <a href="/article/8"><h4 class="txt5 color0-hov trans-0-4 m-b-13">
-                                    </h4></a>
+                                The New Lipstick Range Designed to Flatter Asian Skin                                    </h4></a>
 
                                 <p class="m-b-20">
-                                    "I think of lipstick as clothing for your words"
+                                "I think of lipstick as clothing for your words"
                                 </p>
 
                                 <a href="/article/8" class="txt4">
